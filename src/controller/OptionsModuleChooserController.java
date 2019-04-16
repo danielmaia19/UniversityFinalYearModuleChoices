a8a6c7f76d93e95a2358bf5a1ed25c767502b4f9
Dan Maia <dannymaia1992@gmail.com>
2019-04-16 03:46:23 +0100

    Complete

--- src/controller/OptionsModuleChooserController.java
+++ src/controller/OptionsModuleChooserController.java
@@ -6,7 +6,6 @@ import java.io.*;
 import java.util.*;
 import javafx.event.Event;
 import java.nio.file.Paths;
-import java.nio.file.Files;
 import javafx.stage.FileChooser;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
@@ -15,7 +14,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.beans.property.StringProperty;
-
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -153,69 +151,48 @@ public class OptionsModuleChooserController {
         }
     }
 
-    //private Course[] setupAndRetrieveCourses() {
-    //    Module imat3423 = new Module("IMAT3423", "Systems Building: Methods", 15, true, Delivery.TERM_1);
-    //    Module imat3451 = new Module("IMAT3451", "Computing Project", 30, true, Delivery.YEAR_LONG);
-    //    Module ctec3902_SoftEng = new Module("CTEC3902", "Rigorous Systems", 15, true, Delivery.TERM_2);
-    //    Module ctec3902_CompSci = new Module("CTEC3902", "Rigorous Systems", 15, false, Delivery.TERM_2);
-    //    Module ctec3110 = new Module("CTEC3110", "Secure Web Application Development", 15, false, Delivery.TERM_1);
-    //    Module ctec3426 = new Module("CTEC3426", "Telematics", 15, false, Delivery.TERM_1);
-    //    Module ctec3605 = new Module("CTEC3605", "Multi-service Networks 1", 15, false, Delivery.TERM_1);
-    //    Module ctec3606 = new Module("CTEC3606", "Multi-service Networks 2", 15, false, Delivery.TERM_2);
-    //    Module ctec3410 = new Module("CTEC3410", "Web Application Penetration Testing", 15, false, Delivery.TERM_2);
-    //    Module ctec3904 = new Module("CTEC3904", "Functional Software Development", 15, false, Delivery.TERM_2);
-    //    Module ctec3905 = new Module("CTEC3905", "Front-End Web Development", 15, false, Delivery.TERM_2);
-    //    Module ctec3906 = new Module("CTEC3906", "Interaction Design", 15, false, Delivery.TERM_1);
-    //    Module imat3410 = new Module("IMAT3104", "Database Management and Programming", 15, false, Delivery.TERM_2);
-    //    Module imat3406 = new Module("IMAT3406", "Fuzzy Logic and Knowledge Based Systems", 15, false, Delivery.TERM_1);
-    //    Module imat3611 = new Module("IMAT3611", "Popular Technology Ethics", 15, false, Delivery.TERM_1);
-    //    Module imat3613 = new Module("IMAT3613", "Data Mining", 15, false, Delivery.TERM_1);
-    //    Module imat3614 = new Module("IMAT3614", "Big Data and Business Models", 15, false, Delivery.TERM_2);
-    //    Module imat3428_CompSci = new Module("IMAT3428", "Information Technology Services Practice", 15, false, Delivery.TERM_2);
-    //
-    //    Course compSci = new Course("Computer Science");
-    //    compSci.addModuleToCourse(imat3423);
-    //    compSci.addModuleToCourse(imat3451);
-    //    compSci.addModuleToCourse(ctec3902_CompSci);
-    //    compSci.addModuleToCourse(ctec3110);
-    //    compSci.addModuleToCourse(ctec3426);
-    //    compSci.addModuleToCourse(ctec3605);
-    //    compSci.addModuleToCourse(ctec3606);
-    //    compSci.addModuleToCourse(ctec3410);
-    //    compSci.addModuleToCourse(ctec3904);
-    //    compSci.addModuleToCourse(ctec3905);
-    //    compSci.addModuleToCourse(ctec3906);
-    //    compSci.addModuleToCourse(imat3410);
-    //    compSci.addModuleToCourse(imat3406);
-    //    compSci.addModuleToCourse(imat3611);
-    //    compSci.addModuleToCourse(imat3613);
-    //    compSci.addModuleToCourse(imat3614);
-    //    compSci.addModuleToCourse(imat3428_CompSci);
-    //
-    //    Course softEng = new Course("Software Engineering");
-    //    softEng.addModuleToCourse(imat3423);
-    //    softEng.addModuleToCourse(imat3451);
-    //    softEng.addModuleToCourse(ctec3902_SoftEng);
-    //    softEng.addModuleToCourse(ctec3110);
-    //    softEng.addModuleToCourse(ctec3426);
-    //    softEng.addModuleToCourse(ctec3605);
-    //    softEng.addModuleToCourse(ctec3606);
-    //    softEng.addModuleToCourse(ctec3410);
-    //    softEng.addModuleToCourse(ctec3904);
-    //    softEng.addModuleToCourse(ctec3905);
-    //    softEng.addModuleToCourse(ctec3906);
-    //    softEng.addModuleToCourse(imat3410);
-    //    softEng.addModuleToCourse(imat3406);
-    //    softEng.addModuleToCourse(imat3611);
-    //    softEng.addModuleToCourse(imat3613);
-    //    softEng.addModuleToCourse(imat3614);
-    //
-    //    Course[] courses = new Course[2];
-    //    courses[0] = compSci;
-    //    courses[1] = softEng;
-    //
-    //    return courses;
-    //}
+    /**
+     * Retrieve all courses and modules associated to it by reading from a file with a
+     * certain format.
+     *
+     * @return a set of courses and their modules.
+     */
+    private Set<Course> setupAndRetrieveCourses() {
+
+        // Used Sets to prevent duplicate courses.
+        Set<Course> courses = new HashSet<>();
+        List<String> lines = new ArrayList<>();
+        Course course = null;
+
+        try {
+            File selectedFile = openDialogBuilder("Open Courses", "Open all the courses", "txt");
+            Scanner scan = new Scanner(selectedFile);
+
+            while (scan.hasNextLine()) lines.add(scan.nextLine());
+
+            for (String line : lines) {
+                if (line.split(",").length == 1) course = new Course(line);
+                else {
+                    String[] lineValues = line.split(",");
+                    Module module = new Module(null, null, 0, false, null);
+                    module.setModuleCode(lineValues[0]);
+                    module.setModuleName(lineValues[1]);
+                    module.setCredits(Integer.parseInt(lineValues[2]));
+                    module.setMandatory(module.convertMandatoryToBoolean(lineValues[3].toLowerCase()));
+                    module.setRunPlan(Delivery.convertFromString(lineValues[4]));
+                    if (course != null) {
+                        course.addModuleToCourse(module);
+                    }
+                    courses.add(course);
+                }
+            }
+        } catch (FileNotFoundException error) {
+            alertDialogBuilder(AlertType.ERROR, "Files was not found", null, "File was not found.");
+        } catch (NullPointerException error) {
+            System.out.println("Dialog was cancelled");
+        }
+        return courses;
+    }
 
     /**
      * Add the selected module to the list and increase the credits accordingly
@@ -567,6 +544,7 @@ public class OptionsModuleChooserController {
         public void handle(Event event) {
             model.getAllSelectedModules().clear();
             view.getModuleSelectionViewPane().resetAll();
+            setupAndRetrieveCourses();
 
             // Email validation - check if the email is valid
             if (studentProfileViewPane.isEmailValid()) {
@@ -604,39 +582,8 @@ public class OptionsModuleChooserController {
         @Override
         public void handle(Event event) {
             studentProfileViewPane.clearCombox();
-
-            // Used Sets to prevent duplicate courses.
-            Set<Course> courses = new HashSet<>();
             try {
-                Scanner scan = new Scanner(new File("courses2.txt"));
-                scan.useDelimiter("[,\r\n]+");
-                List<String> lines = new ArrayList<>();
-                Course course = null;
-
-                while(scan.hasNextLine()) lines.add(scan.nextLine());
-
-                for(String line : lines) {
-                    if(line.split(",").length == 1) {
-                        course = new Course(line);
-                    } else {
-                        String[] lineValues = line.split(",");
-                        Module module = new Module(null, null, 0, false, null);
-                        module.setModuleCode(lineValues[0]);
-                        module.setModuleName(lineValues[1]);
-                        module.setCredits(Integer.parseInt(lineValues[2]));
-                        module.setMandatory(module.convertMandatoryToBoolean(lineValues[3].toLowerCase()));
-                        module.setRunPlan(Delivery.convertFromString(lineValues[4]));
-                        if (course != null) {
-                            course.addModuleToCourse(module);
-                        }
-                        courses.add(course);
-                    }
-                }
-
-                studentProfileViewPane.populateComboBoxWithCourses(courses);
-            } catch (IOException error) {
-                error.printStackTrace();
-                alertDialogBuilder(AlertType.ERROR, "Error", null, error.getMessage());
+                studentProfileViewPane.populateComboBoxWithCourses(setupAndRetrieveCourses());
             } catch (IllegalArgumentException error) {
                 alertDialogBuilder(
                         AlertType.ERROR,
